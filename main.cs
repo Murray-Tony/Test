807edf5fbed0f8732706a692e5dc2758ff0e759b
Murray-Tony <85173110+Murray-Tony@users.noreply.github.com>
2021-08-30 17:02:54 +0000

    ;

--- main.cs
+++ main.cs
@@ -3,6 +3,6 @@ using System;
 class Program {
   public static void Main (string[] args) {
     Console.WriteLine ("Hello World");
-    Console.WriteLine ("I am learning to program!"):
+    Console.WriteLine ("I am learning to program!");
   }
 }
